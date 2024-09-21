200999f5323c3caa4e049d6e704e8bfec49cdb44
Druzai <g9code@yandex.ru>
2024-09-21 15:53:17 +0300

    Updated translations to version 24w38a Fixed creating pot-file Updated cryptography

--- locales/generate_pot.py
+++ locales/generate_pot.py
@@ -1,20 +1,27 @@
 from os import path, system, chdir
 from sys import platform, argv
 
+allowed_folders = ['commands', 'components', 'config']
+allowed_files = ['Discord_bot.py', '_frozen_translations.py']
+
 
 def generate_pot_file():
+    input_files_line = (
+        " ".join([f"../{s}" for s in allowed_files]) + " " +
+        " ".join([f"../{f}/*.py" for f in allowed_folders])
+    )
     if platform == "linux" or platform == "linux2":
         system("python3 ./generate_translation_lines.py")
         chdir("locales")
-        system_code = system("pygettext3 -d lang -o lang.pot -v -k get_translation ../*.py ../*/*.py")
+        system_code = system("pygettext3 -d lang -o lang.pot -v -k get_translation " + input_files_line)
     elif platform == "darwin":
         system("python3 ./generate_translation_lines.py")
         chdir("locales")
-        system_code = system(f"python3 pygettext.py -d lang -o lang.pot -v -k get_translation ../*.py ../*/*.py")
+        system_code = system(f"python3 pygettext.py -d lang -o lang.pot -v -k get_translation " + input_files_line)
     elif platform == "win32":
         system("python .\\generate_translation_lines.py")
         chdir("locales")
-        system_code = system(f"python pygettext.py -d lang -o lang.pot -v -k get_translation ..\\*.py ..\\*\\*.py")
+        system_code = system(f"python pygettext.py -d lang -o lang.pot -v -k get_translation " + input_files_line)
     else:
         print("Your system - unknown.")
         return
